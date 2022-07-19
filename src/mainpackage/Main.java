ab09dc98566f68ac617ee4d134f5bbc2e16f4a74
fredaii <alfredolmazza@gmail.com>
2022-07-18 22:37:47 -0300

    Ejercicios tema 1

---
+++ src/mainpackage/Main.java
@@ -0,0 +1,39 @@
+package mainpackage;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Tipos de datos
+
+        // 1. Numéricos enteros
+
+        byte variable1 = 10;
+        short variable2 = 100;
+        int variable3 = 30000;
+        long variable4 = 1122334455;
+
+        // 2. Numéricos decimales
+
+        float variable5 = 1.5f;
+        double variable6 = 0.20;
+
+        // 3. Texto
+
+        String variable7 = "Soy una String";
+        char variable8 = 'A';
+
+        //Variables booleanas
+        boolean variable9 = false;
+        boolean variable10 = true;
+
+        System.out.println("Byte: " + variable1 +
+                           "\nShort: " + variable2 +
+                           "\nInteger: " + variable3 +
+                           "\nLong: " + variable4 +
+                           "\nFloat: " + variable5 +
+                           "\nDouble: " + variable6 +
+                           "\nString: " + variable7 +
+                           "\nChar: " + variable8 +
+                           "\nBoolean: " + variable9 + ", " + variable10);
+    }
+}
